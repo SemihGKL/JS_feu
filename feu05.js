2eb306ea7f7613b069495cf55ec68502a713b7cd
Semih Gokol <gokol.semih@gmail.com>
2023-02-01 14:34:45 +0100

    exo5 last one

--- feu05.js
+++ feu05.js
@@ -67,6 +67,94 @@ else
   end
 end
 
-
-
 */
+
+//initalize variables
+let args = process.argv
+let arg1 = args[2]
+let arg2 = args[3]
+
+const fs = require('fs');
+const encodage = 'utf8'
+let datas
+let begin = []
+
+
+
+//used f()
+
+//permet de recup le contenu d'un fichier
+function getFileContent(fileName){
+    datas = fs.readFileSync(fileName, encodage)
+    return datas
+}
+
+//vérifier la présence d'un fichier dans l'arborescence
+function isNotFile(args) {
+    for (let i = 0; i < args.length; i++) {
+        if (fs.existsSync(args[i]) == true) {
+            //on fait r
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+
+function findPath(plat, start) {
+  
+}
+
+//mainf()
+function labyr(file) {
+  let fileContent = getFileContent(file)
+
+  //get all variables we need
+  const allElems = fileContent.split("\n")
+
+  const nbLgn = allElems[0][0]
+  const nbCln = allElems[0][1]
+  const obstacle = allElems[0][2]
+  const markCarre = allElems[0][3]
+
+  // //récupérer le plateau de jeu uniquement
+  const plateau = allElems.slice(1)
+  console.log(plateau);
+
+
+  //each row
+  for (let i = 0; i < plateau.length; i++) {
+    
+    //each element
+    for (let j = 0; j < plateau[i].length; j++) {
+
+      //on a trouvé l'entrée du labyrinthe
+      if (plateau[i][j] == '2' && plateau[i][j+1] == '$') {
+        begin.push(i,j)
+        findPath(plateau, begin)
+      }
+    }
+  }
+}
+
+
+
+
+
+//error management
+if (arg1 == undefined || arg2 !== undefined) {
+  console.log("Merci d'entrer que 2 arguments valables");
+  return
+}
+
+if (isNotFile(args)) {
+  //good on fait rien
+} else {
+  console.log("Aucun fichier trouvé");
+  return
+}
+
+//result
+
+labyr(arg1);
